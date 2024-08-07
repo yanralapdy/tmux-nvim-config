73fd4c770d8c0ce97e295b809e48e03298d02ecd
yanralapdy <apdy@62teknologi.com>
2024-08-07 17:48:04 +0700

    main, add new key bind

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -6,4 +6,16 @@ function SetColor(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
+function SetIndent()
+    print("===============")
+    print(vim.bo.filetype)
+    print("===============")
+    if vim.bo.filetype == "javascript" then
+        vim.bo.tabstop = 2
+        vim.bo.softtabstop = 2
+        vim.bo.shiftwidth = 2
+    end
+end
+
+SetIndent()
 SetColor()
--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -6,27 +6,23 @@ harpoon:setup()
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
---vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end) 
---vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end) 
---vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end) 
---vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+--vim.keymap.set("n", "<C-H>", function() harpoon:list():select(1) end)
+--vim.keymap.set("n", "<C-T>", function() harpoon:list():select(2) end)
+--vim.keymap.set("n", "<C-N>", function() harpoon:list():select(3) end)
+--vim.keymap.set("n", "<C-S>", function() harpoon:list():select(4) end)
+
 -- Toggle previous & next buffers stored within Harpoon list 
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-N>", function() harpoon:list():next() end)
 
-
---harpoon:setup({})  
+--harpoon:setup({})
 ---- basic telescope configuration 
---local conf = require("telescope.config").values 
---local function toggle_telescope(harpoon_files)     
---	local file_paths = {}     
---	for _, item in ipairs(harpoon_files.items) do         
---		table.insert(file_paths, item.value)     
---	end      
+--local conf = require("telescope.config").values
+--local function toggle_telescope(harpoon_files)
+--	local file_paths = {}
+--	for _, item in ipairs(harpoon_files.items) do
+--		table.insert(file_paths, item.value)
+--	end
 --
 --	require("telescope.pickers").new({}, {
 --		prompt_title = "Harpoon",
@@ -35,6 +31,6 @@ vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 --		}),
 --		previewer = conf.file_previewer({}),
 --		sorter = conf.generic_sorter({}),
---	}):find() 
---end  
+--	}):find()
+--end
 --vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
